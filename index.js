508645467b7fcf9063f220b03038ce13157d02c3
Carlos Zimmerle <carloszimmerle@gmail.com>
2024-05-23 15:00:11 -0300

    refactor: reorganize main logic

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import { Command } from 'commander';
 import path from 'path';
-import { Node } from "ts-morph";
+import util from 'util';
+import fs from 'fs';
+import { exec } from 'child_process';
+const execP = util.promisify(exec);
 
+import { Command } from 'commander';
+import { merge } from 'rxjs';
+
+// local imports
 import processConfigFile from './lib/configProcessing.js';
+import createManager from './lib/metrics/structural/manager.js';
 
-import { computeAMNOI, computeAPXI } from "./lib/metrics/structural/index.js";
+const OUTPUT_DIR = './output/';
+// Cleans the output folder and recreate it
+if (fs.existsSync(OUTPUT_DIR)) {
+  fs.rmdirSync(OUTPUT_DIR, { recursive: true });
+}
+fs.mkdirSync(OUTPUT_DIR);
 
 const program = new Command();
 
 program
-  //.command('path')
-  //.description('Analyze the usability of JavaScript packages')
-  //.argument('<string>', 'path pointing to configuration files')
   .requiredOption('-p, --path <dir>', 'the path pointing to configuration files')
   .parse();
 
@@ -20,32 +29,35 @@ if (path.extname(configFilePath) != ".json") {
   throw new Error("Configuration file must be a JSON file!");
 }
 
-let projects = await processConfigFile(configFilePath);
+const projects = await processConfigFile(configFilePath);
 
-const mainFile = projects.sourceFiles;
+const managers = projects.map(project => createManager(project));
 
-const modules = new Map();
-let modulePath, declarationsMap;
-for (const [name, declarations] of mainFile[0].getExportedDeclarations()) {
-  declarations.forEach(declaration => {
-    if (Node.isClassDeclaration(declaration) || Node.isFunctionDeclaration(declaration)) {
-      //modulePath = path.relative(projects.tsConfigFilePath, declaration.getSourceFile().getFilePath());
-      modulePath = declaration.getSourceFile().getFilePath();
-
-      if (!modules.has(modulePath)) {
-        modules.set(modulePath, new Map());
-      }
-      declarationsMap = modules.get(modulePath);
-
-      if (!declarationsMap.has(name)) {
-        declarationsMap.set(name, []);
-      }
-      let decs = declarationsMap.get(name);
-
-      decs.push(declaration.getStructure());
-    }
-  });
+//install all projects' dependencies
+console.log("Installing projects' dependencies...")
+for (let manager of managers) {
+  try {
+    await execP(`cd ${manager.projectConfig.basePath} && npm install`);
+  } catch (e) { }
+  let { error } = await execP(`cd ${manager.projectConfig.basePath} && npm install type-plus`);
+  if (error) {
+    console.error(error);
+    process.exit();
+  }
 }
 
-let res = await Promise.all([computeAMNOI(modules), computeAPXI(modules)]);
-console.log(res[0].index, res[1].index);
+console.log("Initializing metrics processing...")
+merge(...managers.map(manager => manager.computeMetricsAsObservable()))
+  .subscribe({
+    next: data => {
+      fs.writeFile(path.join(OUTPUT_DIR, `${data.projectName + ' - ' + data.metric}.json`),
+        JSON.stringify(data), 'utf8', (err) => {
+          if (err) {
+            console.error(err);
+            process.exit();
+          }
+        })
+    },
+    error: err => console.error("An error happened during the processing:\n" + err),
+    complete: _ => console.log("Operation completed successfully!\nResults available at:" + OUTPUT_DIR)
+  });
